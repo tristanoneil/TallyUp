e28822f0d9c226b3e0e3caf468194338cfea9343
Tristan O'Neil <tristanoneil@gmail.com>
2014-09-23 14:24:31 -0400

    Add placeholder goals.

--- TallyUp/GoalsController.swift
+++ TallyUp/GoalsController.swift
@@ -11,9 +11,17 @@ import UIKit
 class GoalsController: UITableViewController {
 
     var goals = Goal.allObjects()
+    let realm = RLMRealm.defaultRealm()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        //
+        // Temporarily delete all goals for testing purposes.
+        //
+        realm.beginWriteTransaction()
+        realm.deleteObjects(goals)
+        realm.commitWriteTransaction()
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,5 +50,17 @@ class GoalsController: UITableViewController {
         cell.goalCard.layer.shadowOpacity = 0.1
 
         return cell
+    }
+
+    @IBAction func addGoal(sender: AnyObject) {
+        let goal = Goal()
+        goal.name = "Goal #\(arc4random_uniform(150))"
+
+        realm.beginWriteTransaction()
+        realm.addObject(goal)
+        realm.commitWriteTransaction()
+
+        goals = Goal.allObjects()
+        tableView.reloadData()
     }
 }
